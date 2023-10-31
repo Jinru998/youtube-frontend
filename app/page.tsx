33b1a74e3f7e92a0e28230589015bcff94bc67fc
Jerry Cai <jerrycai@Jerrys-MacBook-Air.local>
2023-10-30 22:44:51 -0400

    initial-commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,106 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import React, { useState } from 'react';
+import axios from 'axios';
+import styles from './page.module.css';
 
 export default function Home() {
+  // State variables for video upload
+  const [file, setFile] = useState<File | null>(null);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [searchTitle, setSearchTitle] = useState<string>('');
+  const [searchResults, setSearchResults] = useState<any[]>([]);
+
+  const handleUpload = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    if (file) {
+      formData.append('video', file);
+    }
+    else {
+      console.error("No file selected");
+      return;
+  
+    }
+    formData.append('title', title);
+    formData.append('description', description);
+
+    try {
+      const response = await axios.post('http://localhost:3000/videos/upload', formData);
+      console.log('Video uploaded:', response.data);
+    } catch (error) {
+      console.error('Error uploading video:', error);
+    }
+  };
+
+  const handleVideoSearch = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3000/videos/search?title=${searchTitle}`);
+      setSearchResults(response.data);
+      console.log("fetch video successfully");
+    } catch (error) {
+      console.error('Error fetching video:', error);
+    }
+  };
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+      {/* Upload Video Section */}
+      <section className={styles.uploadSection}>
+        <h1>Upload Video</h1>
+        <form onSubmit={handleUpload}>
+          <div>
+            <label>Title:</label>
+            <input 
+              type="text" 
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
             />
-          </a>
+          </div>
+          <div>
+            <label>Description:</label>
+            <textarea 
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </div>
+          <div>
+            <input 
+              type="file" 
+              accept="video/*"
+              onChange={(e) => {
+                if (e.target.files && e.target.files.length > 0) {
+                    setFile(e.target.files[0]);
+                }
+            }}           />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </section>
+      <section className={styles.searchSection}>
+        <h1>Search Video</h1>
+        <div>
+          <input 
+            type="text" 
+            value={searchTitle}
+            onChange={(e) => setSearchTitle(e.target.value)}
+            placeholder="Enter video title"
+          />
+          <button onClick={handleVideoSearch}>Search</button>
         </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        {searchResults.map(video => (
+          <div key={video.video_id}>
+            <h2>{video.title}</h2>
+            <p>{video.description}</p>
+            <video controls width="640" height="360">
+              <source src={video.url} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+        ))}
+      </section>
     </main>
-  )
+  );
 }
